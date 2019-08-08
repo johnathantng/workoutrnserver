a310630c3147611a9e20dc23e9f5fb5f2e51607f
johnathantng <johnathan.tng@gmail.com>
2019-08-08 16:43:36 -0700

    added bcrypt.js packages

--- controllers/register.js
+++ controllers/register.js
@@ -13,7 +13,7 @@ const handleRegister = (req, res, db, bcrypt) => {
 			.into('login')
 			.returning('email')
 			.then(loginEmail => {
-				return trx('users')
+				return trx('username')
 					.returning('*')
 					.insert({
 						email: loginEmail[0],
