b11d79b81c820cafeea11ac3a4ba3d0f669123f6
johnathantng <johnathan.tng@gmail.com>
2019-08-07 19:11:39 -0700

    added pg packages

--- server.js
+++ server.js
@@ -7,5 +7,14 @@ const knex = require('knex');
 const app = express();
 app.use(bodyParser.json());
 
+const db = knex({
+	client: 'pg',
+	connection: {
+		host : `${process.env.DB_HOST}`,
+		user : `${process.env.DB_USER}`,
+		password : `${process.env.DB_PASS}`,
+		database : `${process.env.DB_NAME}`,
+	}
+});
 
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
