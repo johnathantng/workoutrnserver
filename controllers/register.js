4d02c728825407e3d323ca747c564be6f4c8ae32
johnathantng <johnathan.tng@gmail.com>
2019-08-09 16:15:25 -0700

    updated so that blank users will no longer register

--- controllers/register.js
+++ controllers/register.js
@@ -2,33 +2,38 @@ const handleRegister = (req, res, db, bcrypt) => {
 	const { password } = req.body;
 	let { username, email } = req.body;
 
-	username = username.toLowerCase();
-	email = email.toLowerCase();
-	const salt = bcrypt.genSaltSync(11);
-	const passHash = bcrypt.hashSync(password, salt);
+	if (!email || !username || !password) {
+		return res.status(400).json('incorrect form submission')
+	} else {
 
-	db.transaction(trx => {
-		trx.insert({
-			password: passHash,
-			email: email,
-			username: username
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-			return trx('users')
-			.insert({
-				email: loginEmail[0],
+		username = username.toLowerCase();
+		email = email.toLowerCase();
+		const salt = bcrypt.genSaltSync(11);
+		const passHash = bcrypt.hashSync(password, salt);
+
+		db.transaction(trx => {
+			trx.insert({
+				password: passHash,
+				email: email,
 				username: username
 			})
-			.then(user => {
-				res.json('Registered user');
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+				.insert({
+					email: loginEmail[0],
+					username: username
+				})
+				.then(user => {
+					res.json('Registered user');
+				})
 			})
+			.then(trx.commit)
+			.catch(trx.rollback)
 		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})
-	.catch(err =>  res.status(400).json('unable to register'));
+		.catch(err =>  res.status(400).json('unable to register'));
+	}
 }
 
 
