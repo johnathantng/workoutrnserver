e894c45d224ef304727bc558d12023549843b922
johnathantng <johnathan.tng@gmail.com>
2019-08-24 16:26:04 -0700

    split workout into its own controller route

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,20 +11,6 @@ const handleProfileGet = (req, res, db) => {
 		.catch(err => res.status(400).json('error getting user'))
 }
 
-const handleWorkoutIdGet = (req, res, db) => {
-	const { id, workout_id } = req.params;
-
-	db('workouts').where({ workout_id: workout_id })
-		.then(data => {
-			if (data.length) {
-				res.json(data[0])
-			} else {
-				res.status(400).json('Not found')
-			}
-		})
-		.catch(err => res.status(400).json('error getting workout'))
-}
-
 const handleProfileCreate = (req, res, db) => {
 	const { id } = req.params;
 	const { gender, age, height, weight } = req.body;
@@ -52,87 +38,7 @@ const handleProfileCreate = (req, res, db) => {
 	}
 }
 
-const handleWorkoutCreate = (req, res, db) => {
-	const { id, exercise } = req.params;
-	const { userName, workoutName, workoutType, workoutReps, workoutSets } = req.body;
-
-	return db('workouts')
-		.insert({
-			id: id,
-			username: userName,
-			workout: workoutName,
-			type: workoutType,
-			current_reps: 0,
-			current_sets: 0,
-			target_reps: workoutReps,
-			target_sets: workoutSets
-		})
-		.then(() => res.json('workout added'))
-		.catch(err => {
-			console.log(err)
-			res.status(400).json('unable to add workout')
-		});
-}
-
-const handleWorkoutGet = (req, res, db) => {
-	const { id, exercise } = req.params;
-
-	db.select('*').from('workouts').where({ id: id })
-		.then(data => {
-			if (data.length) {
-				res.json(data)
-			} else {
-				res.status(400).json('not found')
-			}
-		})
-		.catch(err => res.status(400).json('something went wrong'))
-}
-
-const handleWorkoutUpdate = (req, res, db) => {
-	const { id, workout_id } = req.params;
-	const { 
-		workoutName, 
-		workoutType, 
-		currentReps,
-		currentSets,
-		targetReps, 
-		targetSets 
-	} = req.body;
-
-	return db('workouts').where({ workout_id: workout_id })
-		.update({
-			workout: workoutName,
-			type: workoutType,
-			current_reps: currentReps,
-			current_sets: currentSets,
-			target_reps: targetReps,
-			target_sets: targetSets
-		})
-		.then(() => res.json('workout updated'))
-		.catch(err => {
-			console.log(err)
-			res.status(400).json('unable to update workout')
-		})
-}
-
-const handleWorkoutDelete = (req, res, db) => {
-	const { id, workout_id } = req.params;
-
-	return db('workouts').where({ workout_id: workout_id })
-		.delete()
-		.then(() => res.json('workout deleted'))
-		.catch(err => {
-			console.log(err);
-			res.status(400).json('unable to delete workout');
-		})
-}
-
 module.exports = {
 	handleProfileGet: handleProfileGet,
-	handleWorkoutGet: handleWorkoutGet,
-	handleWorkoutIdGet: handleWorkoutIdGet,
-	handleWorkoutUpdate: handleWorkoutUpdate,
-	handleWorkoutDelete: handleWorkoutDelete,
 	handleProfileCreate: handleProfileCreate,
-	handleWorkoutCreate: handleWorkoutCreate
 };
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 const login = require('./controllers/login');
 const register = require('./controllers/register');
 const profile = require('./controllers/profile');
+const workout = require('./controllers/workout');
 
 const app = express();
 app.use(bodyParser.json());
@@ -27,12 +28,12 @@ const db = knex({
 
 app.get('/', (req, res) => { res.send(database.users) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
-app.get('/profile/:id/workouts', (req, res) => { profile.handleWorkoutGet(req, res, db) });
-app.get('/profile/:id/workouts/:workout_id', (req, res) => { profile.handleWorkoutIdGet(req, res, db) });
-app.put('/profile/:id/workouts/:workout_id', (req, res) => { profile.handleWorkoutUpdate(req, res, db) });
-app.delete('/profile/:id/workouts/:workout_id', (req, res) => { profile.handleWorkoutDelete(req, res, db) });
+app.get('/profile/:id/workouts', (req, res) => { workout.handleWorkoutGet(req, res, db) });
+app.get('/profile/:id/workouts/:workout_id', (req, res) => { workout.handleWorkoutIdGet(req, res, db) });
+app.put('/profile/:id/workouts/:workout_id', (req, res) => { workout.handleWorkoutUpdate(req, res, db) });
+app.delete('/profile/:id/workouts/:workout_id', (req, res) => { workout.handleWorkoutDelete(req, res, db) });
 app.post('/profile/:id', (req, res) => { profile.handleProfileCreate(req, res, db) });
-app.post('/profile/:id/workouts/:exercise', (req, res) => { profile.handleWorkoutCreate(req, res, db) });
+app.post('/profile/:id/workouts/:exercise', (req, res) => { workout.handleWorkoutCreate(req, res, db) });
 app.post('/login', (req, res) => { login.handleLogin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
