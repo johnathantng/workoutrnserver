47e781af5c18568ff95455fbb9b9e7a46620ab8d
johnathantng <johnathan.tng@gmail.com>
2019-08-26 07:38:43 -0700

    fixed req.body variables

---
+++ controllers/meal.js
@@ -0,0 +1,95 @@
+const handleMealCreate = (req, res, db) => {
+	const { id, meal } = req.params;
+	const { userName, carbs, protein, fat, calories } = req.body;
+
+	console.log(req.body);
+
+	return db('meals')
+		.insert({
+			id: id,
+			username: userName,
+			meal: meal,
+			calories: calories,
+			carbs: carbs,
+			protein: protein,
+			fat: fat
+		})
+		.then(() => res.json('meal added'))
+		.catch(err => {
+			console.log(err)
+			res.status(400).json('unable to add meal')
+		});
+}
+
+const handleMealGet = (req, res, db) => {
+	const { id, meal } = req.params;
+
+	db.select('*').from('meals').where({ id: id })
+		.then(data => {
+			if (data.length) {
+				res.json(data)
+			} else {
+				res.status(400).json('not found')
+			}
+		})
+		.catch(err => res.status(400).json('something went wrong'))
+}
+
+const handleMealIdGet = (req, res, db) => {
+	const { id, meal_id } = req.params;
+
+	db('workouts').where({ meal_id: meal_id })
+		.then(data => {
+			if (data.length) {
+				res.json(data[0])
+			} else {
+				res.status(400).json('Not found')
+			}
+		})
+		.catch(err => res.status(400).json('error getting meal'))
+}
+
+const handleMealUpdate = (req, res, db) => {
+	const { id, meal_id } = req.params;
+	const { 
+		meal, 
+		calories, 
+		carbs,
+		protein,
+		fat
+	} = req.body;
+
+	return db('meals').where({ meal_id: meal_id })
+		.update({
+			meal: meal,
+			carbs: carbs,
+			protein: protein,
+			fat: fat,
+			calories: calories
+		})
+		.then(() => res.json('meal updated'))
+		.catch(err => {
+			console.log(err)
+			res.status(400).json('unable to update meal')
+		})
+}
+
+const handleMealDelete = (req, res, db) => {
+	const { id, meal_id } = req.params;
+
+	return db('meals').where({ meal_id: meal_id })
+		.delete()
+		.then(() => res.json('meal deleted'))
+		.catch(err => {
+			console.log(err);
+			res.status(400).json('unable to delete meal');
+		})
+}
+
+module.exports = {
+	handleMealGet: handleMealGet,
+	handleMealIdGet: handleMealIdGet,
+	handleMealUpdate: handleMealUpdate,
+	handleMealDelete: handleMealDelete,
+	handleMealCreate: handleMealCreate
+}
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 
 const login = require('./controllers/login');
+const meal = require('./controllers/meal');
 const register = require('./controllers/register');
 const profile = require('./controllers/profile');
 const workout = require('./controllers/workout');
@@ -31,9 +32,14 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.get('/profile/:id/workouts', (req, res) => { workout.handleWorkoutGet(req, res, db) });
 app.get('/profile/:id/workouts/:workout_id', (req, res) => { workout.handleWorkoutIdGet(req, res, db) });
 app.put('/profile/:id/workouts/:workout_id', (req, res) => { workout.handleWorkoutUpdate(req, res, db) });
+app.get('/profile/:id/meals', (req, res) => { meal.handleMealGet(req, res, db) });
+app.get('/profile/:id/meals/:meal_id', (req, res) => { meal.handleMealIdGet(req, res, db) });
+app.put('/profile/:id/meals/:meal_id', (req, res) => { meal.handleMealUpdate(req, res, db) });
 app.delete('/profile/:id/workouts/:workout_id', (req, res) => { workout.handleWorkoutDelete(req, res, db) });
+app.delete('/profile/:id/meals/:meal_id', (req, res) => { meal.handleMealDelete(req, res, db) });
 app.post('/profile/:id', (req, res) => { profile.handleProfileCreate(req, res, db) });
 app.post('/profile/:id/workouts/:exercise', (req, res) => { workout.handleWorkoutCreate(req, res, db) });
+app.post('/profile/:id/meals/:meal', (req, res) => { meal.handleMealCreate(req, res, db) });
 app.post('/login', (req, res) => { login.handleLogin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
